4e540bf36b6b6598895eae9542ad03e084defff0
Guillermo Flores <gflores@csumb.edu>
2018-08-07 17:46:53 +0000

    Added new html files for the different genres. Added the necessary python for them to be able to link together.

--- neTrailer/main.py
+++ neTrailer/main.py
@@ -12,7 +12,30 @@ class MainPage(webapp2.RequestHandler):
         about_template = the_jinja_env.get_template('Templates/about.html')
         self.response.write(about_template.render())
 
+class ComedyPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('Templates/comedy.html')
+        self.response.write(about_template.render())
+        
+class ActionPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('Templates/action.html')
+        self.response.write(about_template.render())
+        
+class DramaPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('Templates/drama.html')
+        self.response.write(about_template.render())
+
+class ThrillerPage(webapp2.RequestHandler):
+    def get(self):
+        about_template = the_jinja_env.get_template('Templates/thriller.html')
+        self.response.write(about_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/SelectedComedy', ComedyPage),
+    ('/SelectedAction', ActionPage),
+    ('/SelectedDrama', DramaPage),
+    ('/SelectedThriller', ThrillerPage),
 ], debug=True)
